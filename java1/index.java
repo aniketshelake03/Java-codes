de6475e8d96bd1d954b53583be65c1e2bb683061
aniketshelake03 <aniketshelke003@gmail.com>
2025-08-28 11:21:36 +0530

    my second file

---
+++ java1/index.java
@@ -0,0 +1,13 @@
+package Git.java1;
+ class index 
+ {
+   public static void main(String args[])
+   {
+     char x;
+     x = 'B';
+     System.out.println("Capital Alphabet = " +x);
+     x = (char) (x+32);
+     System.out.println("Small Alphabet = " +x);
+   }
+    
+}
